9c930f44cf051c3bb1102da758d8fe15c5ec3407
ramilhagverdiyev <ramil.ax2503@gmail.com>
2024-05-06 22:09:01 +0400

    Product card style modified

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,24 @@
 insert into product (name, description, price, in_stock)
-values ('iphone 12', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac nibh nec consequat. Nam eget varius nibh. Nulla ligula justo, convallis in vulputate a, convallis vitae velit. Aliquam erat volutpat. Mauris vel convallis metus, at ultricies magna. Donec vitae porttitor libero, at euismod odio. Nullam pharetra, turpis vel pharetra egestas, arcu orci placerat metus, a interdum odio ex ac leo.
+values ('MacBook Pro 14', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac nibh nec consequat. Nam eget varius nibh. Nulla ligula justo, convallis in vulputate a, convallis vitae velit. Aliquam erat volutpat. Mauris vel convallis metus, at ultricies magna. Donec vitae porttitor libero, at euismod odio. Nullam pharetra, turpis vel pharetra egestas, arcu orci placerat metus, a interdum odio ex ac leo.
+
+', 1999.00, 2),
+       ('Samsung S22+', 'previous year model' , 780.00, 0),
+       ('iPhone 14 Pro', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac nibh nec consequat. Nam eget varius nibh. Nulla ligula justo, convallis in vulputate a, convallis vitae velit. Aliquam erat volutpat. Mauris vel convallis metus, at ultricies magna. Donec vitae porttitor libero, at euismod odio. Nullam pharetra, turpis vel pharetra egestas, arcu orci placerat metus, a interdum odio ex ac leo.
+
+', 999.00, 10),
+       ('iPhone 15 Pro Max', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac nibh nec consequat. Nam eget varius nibh. Nulla ligula justo, convallis in vulputate a, convallis vitae velit. Aliquam erat volutpat. Mauris vel convallis metus, at ultricies magna. Donec vitae porttitor libero, at euismod odio. Nullam pharetra, turpis vel pharetra egestas, arcu orci placerat metus, a interdum odio ex ac leo.
+
+', 1199.00, 10),
+       ('iPhone 12', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac nibh nec consequat. Nam eget varius nibh. Nulla ligula justo, convallis in vulputate a, convallis vitae velit. Aliquam erat volutpat. Mauris vel convallis metus, at ultricies magna. Donec vitae porttitor libero, at euismod odio. Nullam pharetra, turpis vel pharetra egestas, arcu orci placerat metus, a interdum odio ex ac leo.
 
 ', 890.00, 6),
-       ('Samsung S22+', 'previous year model' , 780.00, 0);
+       ('AirPods Pro', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac nibh nec consequat. Nam eget varius nibh. Nulla ligula justo, convallis in vulputate a, convallis vitae velit. Aliquam erat volutpat. Mauris vel convallis metus, at ultricies magna. Donec vitae porttitor libero, at euismod odio. Nullam pharetra, turpis vel pharetra egestas, arcu orci placerat metus, a interdum odio ex ac leo.
+
+', 399.00, 18),
+       ('Sony WH1000XM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac nibh nec consequat. Nam eget varius nibh. Nulla ligula justo, convallis in vulputate a, convallis vitae velit. Aliquam erat volutpat. Mauris vel convallis metus, at ultricies magna. Donec vitae porttitor libero, at euismod odio. Nullam pharetra, turpis vel pharetra egestas, arcu orci placerat metus, a interdum odio ex ac leo.
+
+', 499.00, 5),
+       ('MacBook Pro 16', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac nibh nec consequat. Nam eget varius nibh. Nulla ligula justo, convallis in vulputate a, convallis vitae velit. Aliquam erat volutpat. Mauris vel convallis metus, at ultricies magna. Donec vitae porttitor libero, at euismod odio. Nullam pharetra, turpis vel pharetra egestas, arcu orci placerat metus, a interdum odio ex ac leo.
+
+', 2499.00, 3)
+;
