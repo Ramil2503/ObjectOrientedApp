a5b421bac9157810691d1c6a40e30c2adc4b3916
ramilhagverdiyev <ramil.ax2503@gmail.com>
2024-05-06 22:34:47 +0400

    Removed unessecary imports

--- src/main/java/org/example/objectorientedapp/repository/ShoppingCart.java
+++ src/main/java/org/example/objectorientedapp/repository/ShoppingCart.java
@@ -1,19 +1,14 @@
 package org.example.objectorientedapp.repository;
 
-import lombok.Data;
-import lombok.Getter;
 import org.example.objectorientedapp.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Repository
 public class ShoppingCart {
     Map<Product, Integer> products;
-//    List<Product> products;
 
     public ShoppingCart() {
         products = new HashMap<>();
