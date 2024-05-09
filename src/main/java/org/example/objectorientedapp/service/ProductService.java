1e201d91ff4d96f7e5c70a877e11bacc9caa63e3
ramilhagverdiyev <ramil.ax2503@gmail.com>
2024-05-09 15:57:21 +0400

    TrackUserAction annotation added and implemented

--- src/main/java/org/example/objectorientedapp/service/ProductService.java
+++ src/main/java/org/example/objectorientedapp/service/ProductService.java
@@ -1,6 +1,7 @@
 package org.example.objectorientedapp.service;
 
 import lombok.AllArgsConstructor;
+import org.example.objectorientedapp.aspect.TrackUserAction;
 import org.example.objectorientedapp.model.Product;
 import org.example.objectorientedapp.repository.ProductRepository;
 import org.example.objectorientedapp.repository.ShoppingCart;
@@ -15,16 +16,19 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final ShoppingCart shoppingCart;
 
-
+    @TrackUserAction
     public List<Product> getAllProducts() { return productRepository.findAll(); }
 
+    @TrackUserAction
     public Map<Product, Integer> getAllProductsInCart() { return shoppingCart.findAll(); }
 
+    @TrackUserAction
     public void addToShoppingCart(Long id, int quantity) {
         Product product = productRepository.findById(id).get();
         shoppingCart.save(product, quantity);
     }
 
+    @TrackUserAction
     public double getTotal() {
         double total = 0;
         for (Map.Entry<Product, Integer> entry : shoppingCart.findAll().entrySet()) {
