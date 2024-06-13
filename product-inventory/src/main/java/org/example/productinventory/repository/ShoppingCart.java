cad43f1336cafb4cd069e381668b1dbeb6702e04
ramilhagverdiyev <ramil.ax2503@gmail.com>
2024-06-13 13:39:08 +0400

    Unnecessary code comments removed

--- product-inventory/src/main/java/org/example/productinventory/repository/ShoppingCart.java
+++ product-inventory/src/main/java/org/example/productinventory/repository/ShoppingCart.java
@@ -8,25 +8,6 @@ import java.util.Map;
 
 @Repository
 public class ShoppingCart {
-//    Map<Product, Integer> products;
-//
-//    public ShoppingCart() {
-//        products = new HashMap<>();
-//    }
-//
-//    public void save(Product product, int quantity) {
-//        products.put(product, quantity);
-//    }
-//
-//    public Map<Product, Integer> findAll() {
-//        Map<Product, Integer> result = new HashMap<>();
-//        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
-//            if (entry.getValue() > 0) {
-//                result.put(entry.getKey(), entry.getValue());
-//            }
-//        }
-//        return result;
-//    }
 
     private final Map<String, Map<Product, Integer>> userCarts = new HashMap<>();
 
